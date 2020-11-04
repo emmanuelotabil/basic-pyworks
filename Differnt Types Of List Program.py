cd037f9a8186a8a8ddca3a41e3d8d37442d05ee1
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-04 15:59:18 +0000

    python practice

--- Differnt Types Of List Program.py
+++ Differnt Types Of List Program.py
@@ -1,22 +1,29 @@
 print('\t\tSummary Table')
+
 nums_strings = ["15","100","55","42"]
 nums_ints = [15,100,55,42]
 nums_floats = [12.3,22.8,1.4,5.2]
 nums_lists = [[1,2,3],[4,5,6],[7,8,9]]
+
 print('The variable nums_strings is a ',type(nums_strings))
 print('It contains the elements ',nums_strings)
 print('the element ',nums_strings[0],' is a ',type(nums_strings[0]))
+
 print('\nThe variable num_ints is a ',type(nums_ints))
 print('It contains the elements ',nums_ints)
 print('The element ',nums_ints[0],' is a ',type(nums_ints))
+
 print('\nThe variable num_floats is a ',type(nums_floats))
 print('It contains the elements ',nums_floats)
 print('The element ',nums_floats[0],' is a ',type(nums_floats[0]))
+
 print('\nThe variable num_lists is a ',type(nums_lists))
 print('It contains the elements ',nums_lists)
 print('The element ',nums_lists[0],' is a ',type(nums_lists[0]))
+
 print('\nNow sorting num_strings and num_ints...')
 nums_strings.sort()
+
 print('\nSorted num_strings: ',nums_strings)
 nums_ints.sort()
 print('\nSorted num_ints: ',nums_ints)
