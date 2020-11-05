19477561dea710d20a4295a05c72ac9bb4384d44
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-05 06:31:22 +0000

    python practice

--- Fibonacci Calculator App.py
+++ Fibonacci Calculator App.py
@@ -1,8 +1,9 @@
 print('\t\tHey there! Welcome to the Fibonacci Calculator App')
 
+#Obtaining user input
 num_fib = int(input('\nHow many digits of fibonacci numbers will you like to compute?: '))
 fib_num = [1,1]
-
+#Calculating the values of fib_num
 for n in range(num_fib-2):
     fib = fib_num[n+1] + fib_num[n]
     fib_num.append(fib)
