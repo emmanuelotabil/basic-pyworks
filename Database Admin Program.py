80f856622e855aba0f6ef7c92488d298e34a5c54
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-07 14:21:53 +0000

    python practice

--- Database Admin Program.py
+++ Database Admin Program.py
@@ -1,8 +1,12 @@
+#Welcome the user
 print('\t\tWelcome to the Database Admin Program.')
+
+#Create empty dictionary containing unsernames and passwords
 log_on_information = {'admin00':'pass0000','admin01':'pass0001','admin02':'pass0002','admin03':'pass0003','admin04':'pass0004'}
 
 user_name = input('Enter your username: ')
 
+#Validate username
 if user_name in log_on_information.keys():
 
     i = list(log_on_information.keys()).index(user_name)
@@ -11,11 +15,12 @@ if user_name in log_on_information.keys():
 
     passkey = input('Enter your password: ')
 
+#Validate password
     if passkey == password:
         print('You are logged in!!')
 
     else:
         print('Password is incorrect. Please provide a right entry.')
-        
+
 else:
     print('Username not found. Please enter a correct username.')
