46d51297bd4de7f939eb63846422976ef11f1c93
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-16 23:05:11 +0000

    python practice

--- Voter registration App.py
+++ Voter registration App.py
@@ -2,3 +2,7 @@ print('Welcome to the Voter Registration App')
 user_name = input('Enter your nme??')
 user_age =  input('How old are you?: ')
 parties = ['Republican','Democratic','Independent','Libertarian','Green']
+if user_age>= 18:
+    print('Congratulations ',user_name, ', you are eligible to vote.')
+    for p in parties:
+        print()
