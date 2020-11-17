99f2bc7f90cc0ae5559093c50e2420f0446f9029
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-17 15:36:00 +0000

    python practice

--- Voter registration App.py
+++ Voter registration App.py
@@ -1,24 +1,29 @@
 print('\t\tWelcome to the Voter Registration App')
 print('\t\t\nThis is a program that will simulate registering to vote.')
 
-user_name = input('Enter your nme??').capitalize()
+user_name = input('\nEnter your name?: ').capitalize()
 
-user_age =  input('How old are you?: ').capitalize()
+user_age =  int(input('\nHow old are you?: '))
 
 parties = ['Republican','Democratic','Independent','Libertarian','Green']
 
 if user_age>= 18:
 
-    print('Congratulations ',user_name, ', you are eligible to vote.')
+    print('\nCongratulations ',user_name, ', you are eligible to vote.')
     
-    print('The list of available parties is showm below: ')
+    print('\nThe list of available parties is showm below: ')
     
     for p in parties:
     
         print('-',p)
     
-    user_party = input('What party will you like to join?: ').capitalize()
+    user_party = input('\nWhat party will you like to join?: ').capitalize()
     if user_party in parties:
     
-        print('Kudos ',user_name,'! you have joined the ',user_party, ' party')
-        print( )
+        print('\nKudos ',user_name,'! you have joined the ',user_party, ' party')
+        if user_party =='Republican' or 'Democratic':
+            print('Your selected party is a major party.')
+        elif user_party == 'Independent':
+            print('You are an independent person.')
+        else:
+            print('Your party is not a major party.')
