b91b4164d106f89635cc430e3346178715c854fd
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-18 07:12:46 +0000

    python practice

--- Number-sorter-app.py
+++ Number-sorter-app.py
@@ -1,3 +1,16 @@
 print('Welcome to the Number Sorter App!')
 running = True
-numbers = 
+while running:
+    numbers_string = input('Enter a series of comma separated numbers: ')
+    numbers_string = numbers_string.replace(' ','')
+    numbers_list = numbers_string.split(',')
+    evens = []
+    odds = []
+    for n in numbers_list:
+        n = int(n)
+        if n % 2 == 0:
+            evens.append(n)
+            print(str(n)+ ' is even')
+        else:
+            odds.append(n)
+            print(str(n)+ ' is odd')
