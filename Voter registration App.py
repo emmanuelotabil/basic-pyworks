daeeb9038b6a37c7953f3c786ef7469fda718cac
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-18 07:44:18 +0000

    python practice

--- Voter registration App.py
+++ Voter registration App.py
@@ -1,3 +1,5 @@
+#You are responsible for writing a program that will simulate registering to vote. If a user is 18 or older, your program will present them with a list of potential political parties to register for. Upon choosing a party, your program will confirm that the user has registered and print a specific message depending on the party joined
+
 #Print Welcome message
 print('\t\tWelcome to the Voter Registration App')
 print('\t\t\nThis is a program that will simulate registering to vote.')
