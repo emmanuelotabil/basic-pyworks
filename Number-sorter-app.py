6f24cc6e74662fa024b9ae7250d28ee667841b96
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-18 07:41:11 +0000

    python practice

--- Number-sorter-app.py
+++ Number-sorter-app.py
@@ -40,9 +40,9 @@ while running:
     for x in odds:
         print('\n-',x)
     
-    user_continue = input('Will you like to continue(y or n): ').lower()
+    user_continue = input('\nWill you like to continue(y or n): ').lower()
     
     if user_continue != 'y':
         running = False
     
-        print('Thank you for using the program.')
+        print('\nThank you for using the program.')
