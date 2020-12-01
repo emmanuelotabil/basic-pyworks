72a72bafb53b732d734bcdaa1d644a99ad931604
Emmanuel Otabil <emmazola9@gmail.com>
2020-12-01 02:34:21 +0000

    python practice

--- Guess my word.py
+++ Guess my word.py
@@ -8,8 +8,29 @@ for key in game_dict.keys():
 game_values = []
 for value in game_dict.values():
     game_values.append(value)
-    
+
 running = True
-while running:
+while running: 
     i = random.randint(0,len(game_keys)-1)
     game_category = game_keys[i]
+
+    n = random.randint(0,len(game_values)-1)
+    game_word = game_dict[game_category][n]
+
+    blank_word = []
+    for letter in game_word:
+        blank_word.append('- ')
+
+    print('You are to pick a ',len(blank_word),'letter word from the ',game_category,' category')
+
+    guess = ''
+    guess_count = 0
+    while guess != game_word:
+        print(blank_word.join())
+        user_input = input('Enter your guess: ')
+        guess_count+=1
+
+        if guess == game_word:
+            print('Congratulations! You won the game in ',guess_count,' tries')
+            
+        break
